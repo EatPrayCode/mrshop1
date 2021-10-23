5e77867d805ce2ec9376cc287946f0b1b209f678
Ashwath Bharadwaj <ashwath.bharadwaj@nineleaps.com>
2021-10-23 08:25:15 +0530

    Widths

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -31,7 +31,7 @@ export class Settings {
 export class AppSettings {
     public settings = new Settings(
         'HouseKey',  // theme name
-        'blue',      // blue, green, red, pink, purple, grey, orange-dark
+        'orange-dark',      // blue, green, red, pink, purple, grey, orange-dark
         1,           // 1 or 2  
         true,        // true = sticky, false = not sticky
         'image',     // default, image, carousel, map, video
