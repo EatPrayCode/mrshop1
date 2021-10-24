c637bf82c25a81bded4f57750a530bfae54b4559
Ashwath Bharadwaj <ashwath.bharadwaj@nineleaps.com>
2021-10-24 18:01:41 +0530

    Fixes

--- src/app/pages/payment-packs/components/payment-packs/payment-packs.component.ts
+++ src/app/pages/payment-packs/components/payment-packs/payment-packs.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentPacksComponent implements OnInit {
 
-  rightList: any = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},];
+  rightList: any = [{}, {}, {}, {},{}, {}, {}, {},{}, {}, {}, {},{}, {}, {}, {},{}, {}, {}, {},{}, {}, {}, {},{}, {}, {}, {},{}, {}, {}, {},{}, {}, {}, {}];
 
   constructor() { }
 
