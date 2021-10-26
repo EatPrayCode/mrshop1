55abe0d7f2a0acebe812aee8d7fecdd15581abf7
Ashwath Bharadwaj <ashwath.bharadwaj@nineleaps.com>
2021-10-26 19:18:28 +0530

    Fixes

--- src/app/layouts/customise-pack/components/view-edit-pack/view-edit-pack.component.ts
+++ src/app/layouts/customise-pack/components/view-edit-pack/view-edit-pack.component.ts
@@ -1,10 +1,11 @@
-import { Component, Inject, OnInit, Optional } from '@angular/core';
+import { Component, Inject, OnInit, Optional, ViewEncapsulation } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-view-edit-pack',
   templateUrl: './view-edit-pack.component.html',
-  styleUrls: ['./view-edit-pack.component.scss']
+  styleUrls: ['./view-edit-pack.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ViewEditPackComponent implements OnInit {
 
