0732901867cc6f384fb9080476a7b2575b2d57a7
Ashwath Bharadwaj <ashwath.bharadwaj@nineleaps.com>
2021-10-30 09:11:10 +0530

    Fixes

--- src/app/layouts/layout.module.ts
+++ src/app/layouts/layout.module.ts
@@ -1,3 +1,4 @@
+import { AuthModule } from './../core/auth/auth.module';
 import { CustomisePackModule } from './customise-pack/customise-pack.module';
 import { SubmitPropertyModule } from './../pages/submit-property/submit-property.module';
 import { NgModule } from '@angular/core';
@@ -8,6 +9,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BlankComponent } from './blank/blank.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,8 @@ import { BlankComponent } from './blank/blank.component';
     CommonModule,
     HttpClientModule,
     SharedModule,
+    // CoreModule,
+    AuthModule,
     CustomisePackModule
   ],
   exports: []
--- src/app/theme/components/user-menu/user-menu.component.ts
+++ src/app/theme/components/user-menu/user-menu.component.ts
@@ -1,5 +1,8 @@
+import { SigninComponent } from './../../../core/auth/components/signin/signin.component';
+import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/app.service';
+import { packsDataSampleDoctor } from 'src/app/mock-data/constants';
 
 @Component({
   selector: 'app-user-menu',
@@ -8,9 +11,32 @@ import { AppService } from 'src/app/app.service';
 })
 export class UserMenuComponent implements OnInit {
 
-  constructor(public appService:AppService) { }
+  constructor(
+    public appService: AppService,
+    public dialog: MatDialog
+    ) { }
 
   ngOnInit() {
+  }
+
+  openLoginDialog(): void {
+    let dialogRef = this.dialog.open(SigninComponent, {
+      hasBackdrop: true,
+      disableClose: false,
+      height: '100vh',
+      minWidth: '90%',
+      position: {
+        right: '0px',
+        bottom: '0px',
+      },
+      data: {
+        pack: packsDataSampleDoctor[0]
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 
 }
